1af0e6a3867e73e48c6b98dc8b988215a78be3ce
Ghost-365 <akzoltoevazat94@gmail.com>
2023-02-27 17:14:45 +0600

    Initial commit

---
+++ app/src/main/java/com/example/taskapp/ui/home/HomeFragment.kt
@@ -0,0 +1,49 @@
+package com.example.taskapp.ui.home
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResultListener
+import androidx.navigation.fragment.findNavController
+import com.example.taskapp.R
+import com.example.taskapp.databinding.FragmentHomeBinding
+import com.example.taskapp.model.Task
+import com.example.taskapp.ui.task.TaskFragment
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setFragmentResultListener(TaskFragment.RESULT_TASK){key, bundle ->
+            val result = bundle.getSerializable("task") as Task
+            Log.e("ololo", "onViewCreated: " + result )
+        }
+
+        binding.fab.setOnClickListener {
+            findNavController().navigate(R.id.taskFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
